c26129eb201faf9435f806cb1714b8282bd355c2
bvhieu <banhieu.work@gmail.com>
2025-08-11 23:13:41 +0700

    abc

--- Main1.java
+++ Main1.java
@@ -1,6 +1,6 @@
 class Main1 {
     public static void main(String[] args) {
         // This is the main method where the program starts
-        System.out.println("Hello, World!");
+        System.out.println("Hello, tao la thuong!");
     }
 }
